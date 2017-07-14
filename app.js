080b0a626684c494dab94024f1325cdf073eb65e
daiiz <da.iz.mg@gmail.com>
2017-07-14 21:39:15 +0900

    add

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ const App = React.createClass({
           key: 'cursor',
           style: { left: self.state.x, top: self.state.y }
         },
-        React.DOM.div({ className: 'indicator'}, `${self.state.x}\n${self.state.y}`)
+        React.DOM.div({ className: 'indicator'}, '')//`${self.state.x}\n${self.state.y}`)
       )
     ])
   }
